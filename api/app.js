ce5720334199dd399c24feb5faa70f801ff3d868
James Mulqueeny <james.mulqueeny@dootrix.com>
2021-04-03 12:38:50 +0100

    Initial api commit adding students routes

---
+++ api/app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv/config");
+
+// Import routes
+const studentsRoute = require("./routes/students");
+
+// Middlewares
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Routes
+app.use("/students", studentsRoute);
+
+// Connect to DB
+mongoose.connect(
+  process.env.DB_CONNECTION,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => console.log("Connected to db")
+);
+
+app.listen(3000);
