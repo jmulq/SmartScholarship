86870f896eb53a6f51f5467bd1235377a5e5453e
James Mulqueeny <james.mulqueeny@dootrix.com>
2021-04-05 22:36:55 +0100

    Add addExaM

--- api/routes/exams.js
+++ api/routes/exams.js
@@ -12,4 +12,19 @@ router.get("/", async (req, res) => {
   }
 });
 
+// Add Exam
+router.post("/", async (req, res) => {
+  const exam = new Exam({
+    examId: req.body.examId,
+    name: req.body.name,
+    availableMarks: req.body.availableMarks,
+    examRecords: [],
+  });
+  try {
+    const savedExam = await exam.save();
+    res.json(savedExam);
+  } catch (error) {
+    res.json({ message: error });
+  }
+});
 module.exports = router;
