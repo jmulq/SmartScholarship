b541c210c705b516cfaaac132294386d701e3fd8
James Mulqueeny <james.mulqueeny@dootrix.com>
2021-04-05 22:37:14 +0100

    Implement add and get exam records data logic

--- api/routes/exams.js
+++ api/routes/exams.js
@@ -27,4 +27,37 @@ router.post("/", async (req, res) => {
     res.json({ message: error });
   }
 });
+
+// Add ExamRecords
+router.post("/:examId", async (req, res) => {
+  try {
+    const examRecords = req.body.examRecords.map((record) => {
+      return {
+        publicKey: record.publicKey,
+        score: record.score,
+      };
+    });
+    const updatedExam = await Exam.updateOne(
+      { examId: req.params.examId },
+      { $set: { examRecords } }
+    );
+    res.json(updatedExam);
+  } catch (error) {
+    res.json({ message: error });
+  }
+});
+
+// Get ExamRecords for Exam
+router.get("/:examId", async (req, res) => {
+  try {
+    const examRecords = await Exam.findOne(
+      { examId: req.params.examId },
+      "examRecords"
+    );
+    res.json(examRecords);
+  } catch (error) {
+    res.json({ message: error });
+  }
+});
+
 module.exports = router;
